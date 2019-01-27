ac8564791dd90371dbfeed0f8013159ffa481c59
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-27 01:22:55 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -32,6 +32,7 @@ class Pokemon
   def alter_hp(new_hp, db)
     binding.pry
     row = Pokemon.find(id, db)
+    binding.pry
     db.execute("UPDATE pokemon SET hp = #{new_hp} WHERE id = (?)", id)
     #db.execute("SELECT hp FROM pokemon WHERE id = (?)", id).flatten
   end
